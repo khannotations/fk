f4d8e074738b94ebec971f53d7a1472247492501
Rafi Khan <faiaz.a.khan@gmail.com>
2012-12-16 01:09:57 -0500

    life progress

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -105,6 +105,18 @@ $(document).ready(function() {
     }
   });
 
+  // Play navigation
+  $(".play_item").click(function() {
+    target = $(this).attr("target");
+    $(".play_content").css({opacity: 1});
+    $(".play_frame").hide();
+    $(".play_frame[class~='"+target+"']").show();
+  });
+
+  $(".play_close").click(function() {
+    $(".play_content").css({opacity: 0});
+  });
+
   // Back/forward button clicked
   window.onpopstate = function(event) {
     l = (event.state && event.state.view) || "";
